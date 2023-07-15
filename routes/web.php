8fa95bbc6bb485ad5e6d0eb416d6bb252d6de358
ricotaffarel <ricotaffarel111@gmail.com>
2023-07-15 21:59:06 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -60,4 +60,5 @@ Route::group(['middleware' => ['auth', 'checkrole:rico@gmail.com']], function ()
     Route::resource('administrator/profile', ProfileController::class)->names('administrator.profile');
     Route::resource('administrator/project-gallery', ProjectGalleryController::class)->names('administrator.project-gallery');
     Route::resource('administrator/service-testimony', ServiceTestimonyController::class)->names('administrator.service-testimony');
+    Route::resource('administrator/service-testimony', ServiceTestimonyController::class)->names('administrator.service-testimony');
 });
